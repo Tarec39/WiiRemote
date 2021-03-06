fe685f95600923cf358b32298eda344059264966
Tarek39 <itcameranian@gmail.com>
2021-03-06 20:28:51 +0900

    [update]確認しやすくした それくらいです。

--- detect_nearby_devices.py
+++ detect_nearby_devices.py
@@ -1,7 +1,14 @@
 import bluetooth
 
-devices = bluetooth.discover_devices(lookup_names=True)
-print("Found {} devices." .format(len(devices)))
+print("Searching for nearby devices.")
+print("wait for a second ...")
 
+devices = bluetooth.discover_devices(lookup_names=True)
+
+print("Found {} devices." .format(len(devices)))
 for address, name in devices:
-  print(" {} - {}".format(address, name))
+  print(" {} - {}".format(name, address))
+
+'''
+Your Wiimote address is {58:BD:A3:B9:2E:72}
+'''
